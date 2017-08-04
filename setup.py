7b88c8d46223d26b212683c0aabb742ac7776db1
apra93 <dd.apra93@gmail.com>
2017-08-03 20:03:58 -0700

    Adding setup.py:

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import (setup, find_packages)
+
+setup(name        = 'PyUtils',
+      version     = '0.0.1',
+      author      = 'apra',
+      packages    = find_packages(),
+      )
